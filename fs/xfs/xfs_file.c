ba3f4acfc7da71cc33df2b374d2c29967e38d6fd
Daan De Meyer <daan@amutable.com>
2026-07-11 16:40:17 +0200

    xfs: allow copy_file_range() with backing files
    The XFS remap and splice operations are safe when passed backing files
    resolved by a stackable filesystem.
    
    Advertise FOP_COPY_FILE_RANGE_BACKING on regular XFS files. Compatible
    translated endpoints can then use xfs_file_remap_range(). Unaligned
    requests and remap operations which return zero use the VFS splice
    fallback. On filesystems without reflink support, an aligned remap
    request continues to return -EOPNOTSUPP.
    
    Signed-off-by: Daan De Meyer <daan@amutable.com>

--- fs/xfs/xfs_file.c
+++ fs/xfs/xfs_file.c
@@ -2102,7 +2102,7 @@ const struct file_operations xfs_file_operations = {
 	.remap_file_range = xfs_file_remap_range,
 	.fop_flags	= FOP_MMAP_SYNC | FOP_BUFFER_RASYNC |
 			  FOP_BUFFER_WASYNC | FOP_DIO_PARALLEL_WRITE |
-			  FOP_DONTCACHE,
+			  FOP_DONTCACHE | FOP_COPY_FILE_RANGE_BACKING,
 	.setlease	= generic_setlease,
 };
 
